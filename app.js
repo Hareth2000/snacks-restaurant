e3eed0056f874fcfa11df9ad8567d100b10e12be
Hareth2000 <harethj18@gmail.com>
2024-12-29 16:50:40 +0300

    done task 2

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-//Q1
+// //Q1
 let userName = prompt("Enter your Name please :");
-let Gender = prompt("Enter Your Gender please :");
-if (Gender == "male") {
+let Gender;
+
+while (true) {
+    Gender = prompt("Enter Your Gender please:");
+    if (Gender === "male" || Gender === "female") {
+        break;
+    } else {
+        alert("Invalid input. Please enter 'male' or 'female'.");
+    }
+}
+
+if (Gender === "male") {
     alert(`Welcome, Mr. ${userName}!`);
-} else if (Gender == "female") {
+} else if (Gender === "female") {
     alert(`Welcome, Ms. ${userName}!`);
-} else {
-    alert(`Welcome,${userName}!`);
 }
 let orderChoice = prompt("Would you like to order shawarma, zinger, or burger");
 alert(`Your ${orderChoice} is being prepared.`);
 
 
 
-//Q2
+// //Q2
 let num = prompt("Enter your number from 0 to 10:");
 switch (Number(num)) {
     case 1:
@@ -46,6 +54,42 @@ switch (Number(num)) {
     default:
         console.log("PLEASE TRY AGAIN");
 }
+
+
+// task two
+//Q1
+for (let i = 0; i <= 5; i++) {
+    alert(i);
+}
+
+//Q2
+let number;
+
+while (true) {
+    number = prompt("Please enter a number between 0 and 100:");
+
+    if (number >= 0 && number <= 100) {
+        break;
+    } else {
+        alert("Invalid input. Please enter a number between 0 and 100.");
+    }
+}
+
+
+alert(`Thank you! You entered a valid number: ${number}`);
+
+//Q3
+
+let number2 = parseInt(prompt("Please enter an integer:"));
+
+let sum = 0;
+
+
+for (let i = 0; i <= number2; i++) {
+  sum += i;
+}
+
+alert(`The sum of integers from 0 to ${number2} is: ${sum}`);
 
 
 
